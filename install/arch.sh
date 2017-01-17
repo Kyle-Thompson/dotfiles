2c55f4168725d5d754c7b47d0bf2f715cb5c0dfb
Kyle Thompson <kyle.thompson228@gmail.com>
2017-01-17 00:21:09 -0500

    Made runable from normal user. Escalates to sudo when needed.

--- install/arch.sh
+++ install/arch.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
 # checks
-if [ "$EUID" -ne 0 ]; then
-    echo "must be root"
-    exit 1
-fi
-
 if ! ping -c 1 google.com > /dev/null 2>&1; then
     echo "no internet connection"
     exit 1
 fi
 
 install () {
-    pacman -S --noconfirm $@
-}
-
-as_user () {
-    sudo -u $SUDO_USER $@
+    sudo pacman -Sq --noconfirm $@
 }
 
 
@@ -24,25 +15,25 @@ as_user () {
 echo "configure git? [Y/n]"
 read -n 1 config_git
 if [ -z "$config_git" ] || [ "$config_git" = "Y" ] || [ "$config_git" = 'y' ]; then
-    as_user ssh-keygen -t rsa -b 4096 -C "kyle.thompson228@gmail.com"
-    as_user ssh-add ~/.ssh/id_rsa
+    ssh-keygen -t rsa -b 4096 -C "kyle.thompson228@gmail.com"
+    ssh-add ~/.ssh/id_rsa
     echo "add id_rsa.pub to github and press [ENTER]"
     read
     install git
-    as_user git config --global user.name "Kyle Thompson"
-    as_user git config --global user.email "kyle.thompson228@gmail.com"
-    as_user git config --global user.editor nvim
+    git config --global user.name "Kyle Thompson"
+    git config --global user.email "kyle.thompson228@gmail.com"
+    git config --global user.editor nvim
 fi
 
 
 # dotfile directory
-as_user mkdir -p ~/.config
-as_user rm -rf ~/.dotfiles
-as_user git clone --quiet git@github.com:Kyle-Thompson/dotfiles.git ~/.dotfiles
+mkdir -p ~/.config
+rm -rf ~/.dotfiles
+git clone --quiet git@github.com:Kyle-Thompson/dotfiles.git ~/.dotfiles
 
 
 # media  TODO: Host media somewhere and fetch it here
-as_user curl -s http://i.imgur.com/SpBfUZi.jpg --create-dirs -o ~/Pictures/Wallpapers/deer.jpg
+curl -s http://i.imgur.com/SpBfUZi.jpg --create-dirs -o ~/Pictures/Wallpapers/deer.jpg
 
 
 # neovim
@@ -65,11 +56,13 @@ ln -s ~/.dotfiles/i3 ~/.config/i3
 # zsh
 install zsh
 chsh -s `which zsh`
+rm ~/.zshrc
 ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
 
 
 # termite
 install termite
+rm -rf ~/.config/termite
 ln -s ~/.dotfiles/termite ~/.config/termite
 
 
