c5b59052d2b8c958a8e3588525f359f37609ca52
Kyle Thompson <kyle.thompson228@gmail.com>
2017-11-19 02:13:53 -0500

    Disabled automatic carriage return for prompt

--- zsh/configs/misc.zsh
+++ zsh/configs/misc.zsh
@@ -1,2 +1,6 @@
-# Use emacs keybindings even if our EDITOR is set to vi
+# Use emacs keybindings even if our EDITOR is set to vi.
 bindkey -e
+
+# Do not force prompt to start on a newline.
+setopt NOPROMPT_CR
+
