004e1fc9f8002f66a61090a8e82af6baf1c9c00a
Kyle Thompson <kyle.thompson228@gmail.com>
2017-11-20 22:46:07 -0500

    Updated

--- sys/prompt_git_status/prompt_git_status.cc
+++ sys/prompt_git_status/prompt_git_status.cc
@@ -15,17 +15,12 @@
 /*
  * TODO
  * - optimize staged/untracked into one call to git status that they both look through.
- * - fetch colors from xresources.
- * - make async / multithreaded
+ * - test if async approach is faster.
  * - add compile time checks to see if:
  *   - Xlib is installed
- *   - Xresources colors exist
  *
  * Future work:
- * - Turn into daemon?
- *   - After cding into git dir, run functions to get info and spawn daemon.
- *   - daemon monitors the state of the git repo and updates internal tracking variables.
- *   - This blocks the need to rerun information retrieval commands for every new prompt.
+ * - Determine way to hold and update git info rather than re-query every time.
  */
 
 #define PIPE_NULL " > /dev/null 2>&1"
@@ -34,21 +29,28 @@ struct RGB {
     int red;
     int green;
     int blue;
+    std::string repr;
 
     RGB(int r, int g, int b)
     : red(r), green(g), blue(b)
-    {}
+    {
+        char buffer[22];
+        sprintf(buffer, "\033[38;2;%d;%d;%dm", red, green, blue);
+        repr = std::string(buffer);
+    }
 
     RGB(const std::string& code) 
     : red(strtol(code.substr(1, 2).c_str(), nullptr, 16))
     , green(strtol(code.substr(3, 2).c_str(), nullptr, 16))
     , blue(strtol(code.substr(5, 2).c_str(), nullptr, 16))
-    {}
-
-    std::string to_string() {
+    {
         char buffer[22];
         sprintf(buffer, "\033[38;2;%d;%d;%dm", red, green, blue);
-        return std::string(buffer);
+        repr = std::string(buffer);
+    }
+
+    operator const char*() {
+        return repr.c_str();
     }
 };
 
@@ -71,6 +73,17 @@ bool has_untracked_files() {
 bool has_unstaged_files() {
     return cmd_status("git status --porcelain | grep -E '\\bM'" PIPE_NULL) == 0;
 }
+
+auto untracked_unstaged() {
+    bool unstaged = false, untracked = false;
+    auto pipe = cmd_pipe("git status --porcelain");
+    for (char buffer[512]; fgets(buffer, 512, pipe.get()) && !unstaged && !untracked;) {
+        if (!strncmp(buffer+1, "M", 1)) unstaged = true;
+        else if (!strncmp(buffer+1, "??", 2)) untracked = true;
+    }
+    return std::make_tuple(untracked, unstaged);
+}
+
 
 int count_ahead(const char* branch){
     char command[256];
@@ -129,30 +142,47 @@ char* get_current_branch() {
     return buffer;
 }
 
-std::vector<RGB> get_colors() {
-    std::vector<RGB> colors;
-    colors.reserve(5);
-
+auto get_colors() {
+    auto white = RGB(255, 255, 255);
+    
     XrmInitialize();
     std::unique_ptr<Display, int(*)(Display*)> dpy(XOpenDisplay(NULL), XCloseDisplay);
-    // TODO: if (!dpy) throw error
+    if (!dpy) return std::make_tuple(white, white, white);
     char* resource_manager = XResourceManagerString(dpy.get());
-    // TODO: if (!resource_manager) throw error
+    if (!resource_manager) return std::make_tuple(white, white, white);
     XrmDatabase db = XrmGetStringDatabase(resource_manager);
-    // TODO: if (!db) throw error
-  
-    // 2 = green, 3 = yellow
-    std::array<const char*, 2> color_codes = {{ "color2", "color3" }};
+    if (!db) return std::make_tuple(white, white, white);
+ 
+    // TODO: clean this up.
+    std::vector<RGB> colors;
     XrmValue ret;
     char* type;
-    for (auto color_code : color_codes) {
+    for (auto& color_code : {"color1", "color2", "color3"}) {
         XrmGetResource(db, color_code, "String", &type, &ret);
-        if (ret.addr != NULL && !strncmp("String", type, 64))
+        if (ret.addr != nullptr && !strncmp("String", type, 64))
             colors.emplace_back(ret.addr);
-        // TODO: else throw error
+        else
+            colors.push_back(white);
     }
 
-    return colors;
+    return std::make_tuple(colors[0], colors[1], colors[2]);
+}
+
+void run() {
+    std::future<bool> unstaged = std::async(has_unstaged_files);
+    std::future<bool> untracked = std::async(has_untracked_files);
+    char* branch = get_current_branch();
+    std::future<std::string> ahead_behind = std::async(get_ahead_behind, branch);
+    
+    auto [red, green, yellow] = get_colors();
+
+    printf("%s[(%s)%s%s]%s ",
+           //unstaged.get() ? yellow : green,
+           unstaged.get() ? yellow.repr.c_str() : green.repr.c_str(),
+           branch,
+           ahead_behind.get().c_str(),
+           untracked.get() ? " ?" : "",
+           reset_color);
 }
 
 int main() {
@@ -162,17 +192,9 @@ int main() {
         return 0;
     }
 
-    auto colors = get_colors();
-
-    std::future<bool> unstaged = std::async(has_unstaged_files);
-    std::future<bool> untracked = std::async(has_untracked_files);
-    char* branch = get_current_branch();
-    std::future<std::string> ahead_behind = std::async(get_ahead_behind, branch);
-
-    printf("%s[(%s)%s%s]%s ",
-           unstaged.get() ? colors[1].to_string().c_str() : colors[0].to_string().c_str(),
-           branch,
-           ahead_behind.get().c_str(),
-           untracked.get() ? " ?" : "",
-           reset_color);
+    try {
+        run();
+    } catch (const std::runtime_error& e) {
+        printf("GIT_PROMPT_ERROR %s", e.what());
+    }
 }
