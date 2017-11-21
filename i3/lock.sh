7e3b5ae8da79eed95c49daec238ede83ab7e905d
Kyle Thompson <kyle.thompson228@gmail.com>
2017-11-20 22:48:39 -0500

    Simplified i3lock script and made it work

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#scrot /tmp/screen.png
-#convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-#convert /tmp/screen.png -paint 3 -modulate 80 /tmp/screen.png
-#convert /tmp/screen.png -paint 3 /tmp/screen.png
-#[[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
-#i3lock -u -e -i /tmp/screen.png
-i3lock # -u -e #-i ~/media/wallpapers/deer.jpg
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+convert /tmp/screen.png ~/.config/i3/lock.png -gravity center -composite /tmp/screen.png
+i3lock -e -u -i /tmp/screen.png
