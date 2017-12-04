07b8008ed1364c9fda143632fd550ddc40809575
Kyle Thompson <kyle.thompson228@gmail.com>
2017-12-04 04:33:14 -0500

    Made launch script more generic.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,4 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar main &
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload main &
+    done
+else
+    polybar --reload main &
+fi
