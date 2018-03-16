8031c60bcf9f640772797f4150e27696bab99dfc
Kyle Thompson <kyle.thompson228@gmail.com>
2018-03-16 17:23:20 -0400

    Added desktop configs.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,6 +3,11 @@
 # Terminate already running bar instances
 killall -q polybar
 
+# Get the bar to use
+computer=$(cat /etc/hostname)
+wm=$(wmctrl -m | head -n 1 | sed "s/^Name:\s//g")
+bar=$(echo $computer-$wm | tr '[:upper:]' '[:lower:]')
+
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
