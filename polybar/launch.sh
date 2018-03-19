c0b6eddf4ca5397c1f52d1df8f608f7ab1dcbe23
Kyle Thompson <kyle.thompson228@gmail.com>
2018-03-19 14:45:57 -0400

    Cleaned and modularized polybar setup.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -13,8 +13,8 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload main &
+        MONITOR=$m polybar --reload $bar &
     done
 else
-    polybar --reload main &
+    polybar --reload $bar &
 fi
