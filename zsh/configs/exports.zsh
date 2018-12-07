cd2de3faabe29b0c90b47b41c2c72028b9cfec2d
Kyle Thompson <kyle.thompson228@gmail.com>
2018-12-07 15:32:19 -0500

    Fixed polybar eth export.

--- zsh/configs/exports.zsh
+++ zsh/configs/exports.zsh
@@ -12,4 +12,4 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export VISUAL=nvim
 
 # polybar
-export POLYBAR_ETH_INTERFACE="${$(ip a | grep "[1-9]: enp" | head -n 1 | awk '{print $2}'): : -1}"
+export POLYBAR_ETH_INTERFACE="$($(ip a | grep "[1-9]: enp" | head -n 1 | awk '{print $2}'): : -1)"
