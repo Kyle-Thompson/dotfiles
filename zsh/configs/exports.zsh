8692e2db11246f87efaaa5b7bdea51b36534ee8c
Kyle Thompson <kyle.thompson228@gmail.com>
2018-12-10 21:01:45 -0500

    Attempt to fix POLYBAR_ETH_INTERFACE again and made fzf use rg.

--- zsh/configs/exports.zsh
+++ zsh/configs/exports.zsh
@@ -12,4 +12,7 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export VISUAL=nvim
 
 # polybar
-export POLYBAR_ETH_INTERFACE="$($(ip a | grep "[1-9]: enp" | head -n 1 | awk '{print $2}'): : -1)"
+export POLYBAR_ETH_INTERFACE="${$(ip a | grep "[1-9]: enp" | head -n 1 | awk '{print $2}'): : -1}"
+
+# fzf
+FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
