e142eab12c84db010a9d2b7a484e8cf22686e30a
Kyle Thompson <kyle.thompson228@gmail.com>
2019-03-05 09:10:01 -0500

    Added alias to update pacman mirrors.

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -3,3 +3,5 @@ alias ls='ls --color=auto'
 
 alias ni=nvim
 alias nif='ni $(rg --files | fzf || echo +qa)'
+
+alias update_pacman_mirrors='reflector --sort rate --protocol https --fastest 50 --number 100 --save /etc/pacman.d/mirrorlist'
