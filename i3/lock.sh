5b0e308c794192a3a1577296f5542467658ce426
Kyle Thompson <kyle.thompson228@gmail.com>
2019-03-04 22:53:28 -0500

    Remove blur effect.

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-convert /tmp/screen.png ~/.config/i3/lock.png -gravity center -composite /tmp/screen.png
-i3lock -e -u -i /tmp/screen.png
+i3lock -c 161b1d
