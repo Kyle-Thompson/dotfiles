4011f7421e4c7a7199441ab6db0420e3ad9d68c5
Kyle Thompson <kyle.thompson228@gmail.com>
2019-09-05 23:43:39 -0400

    added transmission aliases

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -5,3 +5,6 @@ alias ni=nvim
 alias nif='ni $(rg --files | fzf || echo +qa)'
 
 alias update_pacman_mirrors='reflector --sort rate --protocol https --fastest 50 --number 100 --save /etc/pacman.d/mirrorlist'
+
+alias transd='transmission-daemon --paused --download-dir ~/media/downloads'
+alias transcli='transmission-remote-cli'
