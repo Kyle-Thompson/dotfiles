66a5454ed410f1aa5f9701683d41e4420005b2d0
Kyle Thompson <kthompson@tower-research.com>
2019-09-06 11:21:29 -0400

    nif accepts multiple files
    Change-Id: I5846ae4373bc29c60896b3aada0a4c035924ba4d

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -2,7 +2,7 @@
 alias ls='ls --color=auto'
 
 alias ni=nvim
-alias nif='ni $(rg --files | fzf || echo +qa)'
+alias nif='ni $(rg --files | fzf -m || echo +qa)'
 
 alias update_pacman_mirrors='reflector --sort rate --protocol https --fastest 50 --number 100 --save /etc/pacman.d/mirrorlist'
 
