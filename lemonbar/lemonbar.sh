6547c429e618755b0330d86acde9d421a76cc9cc
Kyle Thompson <kyle.thompson228@gmail.com>
2019-09-08 13:43:24 -0400

    simplified lemonbar.sh modules and made the script POSIX compliant

--- lemonbar/lemonbar.sh
+++ lemonbar/lemonbar.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # ==============================================================================
 # ======================   Setup   =============================================
@@ -7,130 +7,93 @@
 killall -q lemonbar
 
 fifo_pipe=/tmp/lemonbar_refresh
-if [[ ! -p $fifo_pipe ]]; then
-  mkfifo $fifo_pipe
-fi
+[ -p $fifo_pipe ] && mkfifo $fifo_pipe
 
 wm_name=$(wmctrl -m             \
   | awk -F: 'NR==1 {print $2}'  \
   | sed -e 's/\ //'             \
   | tr '[:upper:]' '[:lower:]')
 
-# number_of_displays=$(xrandr -d :0 -q \
-#   | rg ' connected'                  \
-#   | wc -l                            \
-#   | xargs echo -1+                   \
-#   | bc)
 number_of_displays=$(xrandr -d :0 -q \
   | rg ' connected'                  \
-  | wc -l)
-number_of_displays="$(($number_of_displays-1))"
+  | wc -l                            \
+  | xargs echo -1+ | bc)
 
 # ==============================================================================
 # ======================   Utilities   =========================================
 # ==============================================================================
 
-function color() { echo $(xrdb -query | rg color$1: | awk '{print $2}') }
+color() { xrdb -query | rg color"$1": | awk '{print $2}'; }
 
-function log() { echo $(date +'%Y/%m/%d-%H:%M:%S') - "$@" >> /tmp/lemon.log }
+log() { echo "$(date +'%Y/%m/%d-%H:%M:%S')" - "$@" >> /tmp/lemon.log; }
 
-function join_by { local IFS="$1"; shift; echo "$*"; }
+refresh() { for mod in $@; do mod_"$mod"; done }
 
-# TODO: make clock daemon
-function refresh() { mod_clock; for mod in "$@"; do mod_$mod; done }
-
-function generate_bar() {
-  # seq 0 $number_of_displays \
-  #   | awk '{ printf "\%{S%d}'$(${wm_name}_bar)'\n", $1 }' \
-  #   | xargs join_by ' '
-  for i in $(seq 0 $number_of_displays); do
-    final_bar="${final_bar}%{S$i}$(${wm_name}_bar)"
-  done
-  
-  echo $final_bar
+generate_bar() {
+  bar=$("${wm_name}"_bar)
+  echo $(seq 0 "$number_of_displays" | sed "s/.*/%{S&}$bar/")
 }
+
+# ==============================================================================
+# ======================   Daemons   ===========================================
+# ==============================================================================
+
+# TODO: vpn
+# TODO: internet connection
+# TODO: network device (ethernet/wifi) 
+# TODO: workspace
+# TODO: active window
+# TODO: clock
 
 # ==============================================================================
 # ======================   Modules   ===========================================
 # ==============================================================================
 
-function mod_clock() {
+mod_clock() {
   clock="%{F$(color 4)}$(date +'%H:%M') %{F$(color 7)}$(date +'%b %d')"
 }
 
-function mod_internet() {
-  #TODO: trigger updates on vpn and connection status change
-  internet=""
-
+mod_internet() {
   # vpn status
-  if [[ $(nordvpn status | rg Status | cut -d' ' -f2) == Connected ]]; then
-    internet="$internet%{F$(color 4)}"
+  if [ "$(nordvpn status | rg Status | cut -d' ' -f2)" = "Connected" ]; then
+    vpn="%{F$(color 4)}"
   fi
 
   # connection type
-  local connection=$(route | rg '192.168.1.0' | awk '{ print $NF }' | head -c 1)
-  case $connection in
-    e) internet="${internet}ethernet" ;;
-    w) internet="${internet}wifi"     ;;
-    *) internet="${internet}??"       ;;
+  case "$(route | rg '192.168.1.0' | awk '{ print substr($NF,1,1) }')" in
+    e) connection="ethernet" ;;
+    w) connection="wifi"     ;;
+    *) connection="??"       ;;
   esac
 
   # reset colors and assign to internet
-  internet="$internet%{F$(color 7)}"
+  internet="${vpn}${connection}%{F$(color 7)}"
 }
 
-function mod_workspaces() {
-  workspaces=""
-  local current_workspace=$(wmctrl -d | rg '\*' | awk '{print $1}')
-  for index ws in $(wmctrl -d | awk '{print NR-1,$NF}'); do
-    ws_fmt="%{A:wmctrl -s $index && refbar workspace windows:}$ws%{A}"
-    if [ "$index" -eq "$current_workspace" ]; then
-      workspaces="$workspaces %{F$(color 4)}$ws_fmt%{F$(color 7)}"
-    else
-      workspaces="$workspaces $ws_fmt"
-    fi
-  done
-  # ws_fmt="\%{A:wmctrl -s %d && refbar workspace windows:}%s\%{A}\n"
-  # color_wrap="%{F$(color 4)}&%{F$(color 7)}"
-  # workspaces=$(wmctrl -d \
-  #             | awk '{printf '$ws_fmt', NR-1, $NF}' \
-  #             | sed $current_workspace's/.*/'$color_wrap'/' \
-  #             | xargs join_by ' ')
-  log $workspaces
+mod_workspaces() {
+  curr_ws=$(wmctrl -d | rg '\*' | awk '{print $1}')
+  workspaces=$(wmctrl -d \
+    | awk '{printf "%%{A:wmctrl -s %d && refbar workspaces windows:}%s%%{A}\n", NR-1, $NF}' \
+    | sed $((curr_ws+1))"s/.*/%{F$(color 4)}&%{F$(color 7)}/")
 }
 
 # Display the current active windows for this workspace
 # TODO: Accent the current window. (How to cover all cases of selection?)
-function mod_windows() {
-  windows=""
-  function get_window_name() {
-    echo $(xdotool getwindowname $1 | awk -F- '{print $NF}' | sed -e 's/\ //')
-  }
-  function button_command() {
-    echo wmctrl -i -a $1
-  }
-
+mod_windows() {
   workspace=$(wmctrl -d | rg '\*' | cut -d' ' -f1)
-  window_ids=$(wmctrl -l \
-              | awk '{ if ($2 == w) { print } }' w=$workspace \
-              | cut -d' ' -f1)
-
-  [[ -z $window_ids ]] && return
-
-  #TODO: write into array and python-join over " "
-  while read id; do
-    windows="${windows}%{A:$(button_command $id):}$(get_window_name $id)%{A} "
-  done <<< $window_ids
+  windows=$(wmctrl -l \
+    | awk '{ if ($2 == w) printf "%%{A:wmctrl -i -a %s:}%s%%{A}\n", $1, $NF }' \
+      w="$workspace")
 }
 
 # ==============================================================================
 # ======================   WM Specific   =======================================
 # ==============================================================================
 
-function openbox_initialize_modules() {
+openbox_initialize_modules() {
   refbar workspaces windows clock internet
 }
-function openbox_bar() {
+openbox_bar() {
   echo %{l}${workspaces} ${windows}%{c}${clock}%{r}${internet}
 }
 
@@ -138,19 +101,16 @@ function openbox_bar() {
 # ======================   Run   ===============================================
 # ==============================================================================
 
-${wm_name}_initialize_modules
+"${wm_name}"_initialize_modules
 
-while read line < $fifo_pipe; do
-  refresh $line
-  # TODO: different behavior from calling generate_bar directly?
-  bar=$(generate_bar)
-  log $bar
-  echo $bar
+while read -r line < $fifo_pipe; do
+  refresh "$line"
+  generate_bar
 done \
   | lemonbar \
       -p \
       -f 'Gohu GohuFont' \
-      -B $(color 0) \
+      -B "$(color 0)" \
       -a 100 \
       -g 5760x20+0+0 \
-  | while read line; do eval $line; done
+  | while read -r line; do eval "$line"; done
