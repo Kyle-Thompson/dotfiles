c3bdbc8b4f007e7b72a55598c4d1134482968aed
Kyle Thompson <kyle.thompson228@gmail.com>
2019-12-25 18:19:21 -0500

    tweaked aliases

--- zsh/configs/aliases.zsh
+++ zsh/configs/aliases.zsh
@@ -4,9 +4,9 @@ alias ls='ls --color=auto'
 alias ni=nvim
 alias nif='ni $(rg --files | fzf -m || echo +qa)'
 
-alias update_pacman_mirrors='reflector --sort rate --protocol https --fastest 50 --number 100 --save /etc/pacman.d/mirrorlist'
+alias update_pacman_mirrors='sudo reflector --sort rate --protocol https --fastest 50 --number 100 --save /etc/pacman.d/mirrorlist'
 
-alias transd='transmission-daemon --paused --download-dir ~/media/downloads'
+alias transd='transmission-daemon --paused --download-dir ~/mda/downloads'
 alias transcli='transmission-remote-cli'
 
 alias fehf='feh -.'
