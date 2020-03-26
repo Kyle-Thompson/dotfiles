ef850e1b9f3e64bef34e155e23d9934b821f7b53
Kyle Thompson <kyle.thompson228@gmail.com>
2020-03-26 00:07:13 -0400

    [bin] added a day/night script for screen brightness and redshift

---
+++ bin/screen.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if test -z $1; then
+  echo must provide argument of either day or night
+  exit 1
+fi
+
+case "$1" in
+  day)
+    redshift -x
+    xbacklight -set 80
+    ;;
+  night)
+    redshift -P -O 1500
+    xbacklight -set 10
+    ;;
+  *)
+    echo must provide argument of either day or night
+esac
