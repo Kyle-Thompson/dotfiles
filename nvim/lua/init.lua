e164824fbd4ff795bb7ed44977f8e49fbb80526e
Kyle Thompson <kyle.thompson228@gmail.com>
2021-08-01 19:32:38 -0400

    [nvim] initial dap config

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -201,6 +201,37 @@ require('compe').setup {
 }
 
 
+-- ===================== debugger
+local dap = require('dap')
+dap.adapters.python = {
+  type = 'executable';
+  command = '/usr/bin/python';
+  args = { '-m', 'debugpy.adapter' };
+}
+
+dap.configurations.python = {
+  {
+    -- needed for nvim-dap
+    type = 'python';
+    request = 'launch';
+    name = "Launch file";
+
+    -- debugpy options
+    program = "${file}";
+    pythonPath = function()
+      local root_dir = vim.lsp.buf.list_workspace_folders()[1]
+      if vim.fn.executable(root_dir .. '/venv/bin/python') == 1 then
+        return root_dir .. '/venv/bin/python'
+      elseif vim.fn.executable(root_dir .. '/.venv/bin/python') == 1 then
+        return root_dir .. '/.venv/bin/python'
+      else
+        return '/usr/bin/python'
+      end
+    end;
+  },
+}
+
+
 -- ===================== language server
 lsp.clangd.setup {
   cmd = {'clangd', '--background-index', '--clang-tidy'};
