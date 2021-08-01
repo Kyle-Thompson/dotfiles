08d292ba44df1891967d2dd9719af1c8ef1bf91c
Kyle Thompson <kyle.thompson228@gmail.com>
2021-08-01 19:32:55 -0400

    [nvim] lua language server

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -252,6 +252,48 @@ lsp.rust_analyzer.setup {
   };
 }
 
+local system_name
+if vim.fn.has("mac") == 1 then
+  system_name = "macOS"
+elseif vim.fn.has("unix") == 1 then
+  system_name = "Linux"
+elseif vim.fn.has('win32') == 1 then
+  system_name = "Windows"
+else
+  print("Unsupported system for sumneko")
+end
+
+-- TODO: use some way of getting home dir. this isn't portable
+local sumneko_root_path = '/home/kyle/pkg/lua-language-server'
+local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+require'lspconfig'.sumneko_lua.setup {
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- get the language server to recognize the 'vim' global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- make the server aware of neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 -- diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
