c36b2b6b45b0adc8e023759d2acfbc820477bb9d
Kyle Thompson <kyle.thompson228@gmail.com>
2023-01-22 23:50:42 -0500

    [nvim] fix cmp_nvim_lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -244,8 +244,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp')
-  .update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- ===================== debugger
 map('n', leader..'dc', require'dap'.continue)
